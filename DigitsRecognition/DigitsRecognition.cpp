6aba76afd0625ed4120cafac136a6a5a92fcc118
BartezMadej <BartMadej98@gmail.com>
2020-08-03 14:04:02 +0200

    Neruon implementation added

--- DigitsRecognition/DigitsRecognition.cpp
+++ DigitsRecognition/DigitsRecognition.cpp
@@ -2,8 +2,12 @@
 //
 
 #include <iostream>
-
+#include "Neuron.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+	Neuron* n1 = new Neuron(0.5);
+	std::cout<<"Neuron value:\t"<<n1->getValue()<< std::endl;
+	std::cout<<"Activated value:\t"<<n1->getActivatedVal()<< std::endl;
+	std::cout<<"Derived value:\t"<<n1->getDerivedVal()<< std::endl;
+	return 0;
 }
