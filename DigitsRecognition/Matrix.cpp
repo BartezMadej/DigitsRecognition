179a746fa82bf65cae391ae15efe7ccb41fddbb1
BartezMadej <BartMadej98@gmail.com>
2020-08-09 16:42:54 +0200

    Code refactoring

--- DigitsRecognition/Matrix.cpp
+++ DigitsRecognition/Matrix.cpp
@@ -42,37 +42,37 @@ void Matrix::zerosMatrix()
 	}
 }
 
-std::shared_ptr<Matrix> Matrix::transposeMatrix()
+Matrix Matrix::transposeMatrix()
 {
-	std::shared_ptr<Matrix>pTrans = std::make_shared<Matrix>(this->nCols, this->nRows, false);
+	Matrix trans(this->nCols, this->nRows, false);
 	for (unsigned i = 0; i < nCols; ++i)
 		for (unsigned j = 0; j < nRows; ++j)
-			pTrans->setValue(i, j, this->getValue(j, i));
-	return pTrans;
+			trans.setValue(i, j, this->getValue(j, i));
+	return trans;
 }
 
-std::shared_ptr<Matrix> Matrix::operator*(const std::shared_ptr<Matrix> mat)
+Matrix Matrix::operator*(const Matrix mat)
 {
-	if (this->nCols != mat->getNumRows())
+	if (this->nCols != mat.getNumRows())
 		throw std::logic_error("Matrix operator*");
-	std::shared_ptr<Matrix> res = std::make_shared<Matrix>(this->getNumRows(), mat->getNumCols(),false);
+	Matrix res(this->getNumRows(), mat.getNumCols(),false);
 	for (unsigned i = 0; i < nRows; ++i)
 	{
-		for (unsigned j = 0; j < mat->getNumCols(); ++j)
+		for (unsigned j = 0; j < mat.getNumCols(); ++j)
 		{
-			for (unsigned z = 0; z < mat->getNumRows(); ++z)
-				res->setValue(i, j, res->getValue(i, j) + (this->getValue(i, z) * mat->getValue(z, j)));
+			for (unsigned z = 0; z < mat.getNumRows(); ++z)
+				res.setValue(i, j, res.getValue(i, j) + (this->getValue(i, z) * mat.getValue(z, j)));
 		}
 	}
 	return res;
 }
 
-std::shared_ptr<Matrix> Matrix::operator+(const std::shared_ptr<Matrix> add)
+Matrix Matrix::operator+(const Matrix add)
 {
-	std::shared_ptr<Matrix> res = std::make_shared<Matrix>(this->getNumRows(), add->getNumCols(), false);
+	Matrix res(this->getNumRows(), add.getNumCols(), false);
 	for (unsigned i = 0; i < this->getNumRows(); ++i)
 		for (unsigned j = 0; j < this->getNumCols(); ++j)
-			res->setValue(i, j, this->getValue(i,j) + add->getValue(i, j));
+			res.setValue(i, j, this->getValue(i,j) + add.getValue(i, j));
 	return res;
 }
 
